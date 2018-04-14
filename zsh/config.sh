7cc453d20310d3e3ce7938a42bfd3bf3eed30cba
dumitru <panagdu@debbie.panasys>
2018-04-14 09:26:40 +0100

    Add zsh config and a debian wallpaper/icon

--- zsh/config.sh
+++ zsh/config.sh
@@ -3,3 +3,7 @@ sudo apt-get install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # change the default theme
+
+#change shell
+chsh -s $(which zsh)
+
